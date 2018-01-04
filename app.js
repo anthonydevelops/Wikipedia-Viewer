8e6bdd777fc41a21dd0dd163d9d4fa4ed9bc597f
Anthony Campos <anthonydevelops@gmail.com>
2018-01-03 21:05:50 -0800

    Fixed CORS issue with Fetch api. Display data now.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.addEventListener("keypress", (event) => {
 });
 
 function validateSearch() {
-  let input = document.querySelector("#search").value;
+  const input = document.querySelector("#search").value;
   //Check if search field is empty
   if (input == null || input == "") {
     //Log empty search field error message to the user
@@ -19,15 +19,32 @@ function validateSearch() {
     wikiError.appendChild(errorMsg);
   }
   else {
-    accessWiki(input);
+    const encodedQuery = encodeURIComponent(input);
+    accessWiki(encodedQuery);
   }
 }
 
 function accessWiki(userQuery) {
-  const encodedQuery = encodeURIComponent(userQuery);
-  const url = "https://en.wikipedia.org/w/api.php?action=query&titles=" + encodedQuery +
-  "&prop=revisions&rvprop=content&format=json";
-  fetch(url).then((resp) => resp.json()).then(function(data) {
-    
-  })
+  //Use api to gather title, summary, and urls of related content
+  const url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search="
+  + userQuery + "&origin=*";
+  fetch(url).then(resp => resp.json()).then((data) => {
+    //Arrays to hold query results
+    let queryTitle = [];
+    let queryExcerpt = [];
+    let queryUrl = []
+    for(let i = 0; i < data[1].length; i++) {
+      //Copy over data
+      queryTitle = data[1].slice();
+      queryExcerpt = data[2].slice();
+      queryUrl = data[3].slice();
+    }
+    formatSearch(queryTitle, queryExcerpt, queryUrl);
+  }).catch((error) => {
+    console.log('error is', error)
+  });
+}
+
+function formatSearch(queryTitle, queryExcerpt, queryUrls) {
+
 }
